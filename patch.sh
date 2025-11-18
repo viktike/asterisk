191a5de4f090ace21680300b32a40a605c8c2882
Super User <root@localhost.localdomain>
2025-11-18 08:45:39 +0100

    Add various malloc() implementations.

--- patch.sh
+++ patch.sh
@@ -30,8 +30,25 @@ sudo ./contrib/scripts/install_prereq install
 #    app_konference.so
 #    app_tiresias.so
 #    app_voicechanger.so
+#    chan_capi.so
 #    chan_dongle.so
 #    chan_quectel.so
 #    chan_sccp.so
 #    res_speech_vosk.so
-#
+
+
+########
+# RHEL #
+# ######
+#dnf install jemalloc jemalloc-devel
+#LDFLAGS="-L/usr/lib64 -ljemalloc -Wl,--no-as-needed" ./configure
+#LDFLAGS="-L/usr/lib64 -ljemalloc -Wl,--no-as-needed" make -j4 install
+#MALLOC_CONF="narenas:1" asterisk -cvvv
+
+##########
+# Debian #
+##########
+#apt-get install libjemalloc2 libjemalloc2-dev libgoogle-perftools-dev
+#LDFLAGS="-L/lib/x86_64-linux-gnu -ltcmalloc -Wl,--no-as-needed" ./configure
+#LDFLAGS="-L/lib/x86_64-linux-gnu -ltcmalloc -Wl,--no-as-needed" make -j4 install
+#MALLOC_CONF="narenas:1" TCMALLOC_RELEASE_RATE="0.1" TCMALLOC_MAX_TOTAL_THREAD_CACHE_BYTES="268435456 asterisk -cvvv
